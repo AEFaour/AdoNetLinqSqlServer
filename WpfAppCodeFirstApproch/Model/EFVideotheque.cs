3913db70f09e79f58c61e30acf556df96e7e9289
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-06-29 15:00:54 +0200

    Create Database

---
+++ WpfAppCodeFirstApproch/Model/EFVideotheque.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCodeFirstApproch.Model
+{
+    // Cette class sera notre "Conteneur de Données" -- DataContext
+    class EFVideotheque : DbContext
+    {
+        // Définir les DbSet --> les classes qui seront mappées
+
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Categorie> Categories { get; set; }
+
+        // utiliser une chaine de connexion par appel du constructeur
+
+        // La Chaine de connexion sous permet d'avoir le chemin vers la base de donnée
+
+        public EFVideotheque() : base("ChaineCnx")
+        {
+
+        }
+    }
+
+
+}
---
+++ WpfAppCodeFirstApproch/Model/Film.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCodeFirstApproch.Model
+{
+    class Film
+    {
+        [Key]
+        // Puisque l'attribut contient Id, normalement c'est implicite
+        // /!\ : Parfois ne fonctionne pas --bug
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Resume { get; set; }
+        public int Annee { get; set; }
+
+        // L'association va se traduire par une propriété de navigation
+
+        public virtual ICollection<Categorie> Categories { get; set; }
+    }
+}
