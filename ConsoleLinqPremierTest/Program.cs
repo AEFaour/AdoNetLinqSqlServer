ba632558d3a7bbe2ce661ed5b67bc074505d3f46
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-06-29 11:22:45 +0200

    init Linq

---
+++ ConsoleLinqPremierTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinqPremierTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tab
+            int[] _tab = { 12, 15, 18, 19, 20, 8, 11 };
+
+
+            // Les éléments du tab qui sont > 15
+            // Leur nombre ? 
+
+            var _result = from elt in _tab
+                          where elt > 15
+                          select elt;
+
+            // Pout le moment le requette n'est pas executée 
+            // Pour l'executer il faut parcourir la liste 
+
+            foreach (var item in _result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey(); // Figer l'affichage
+        }
+    }
+}
---
+++ WpfListeFichierRepertoire/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfListeFichierRepertoire
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangerFichier_Click(object sender, RoutedEventArgs e)
+        {
+            //Folderbrowser pour parcourrir les repertoires 
+            //A importer depuis la bibliothèque Forms
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //Un descriptif pour la boîte de dialogue
+            fbd.Description = "Selectionner un repertoire";
+            //Un repertoire de départ (de base)
+
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            DialogResult result = fbd.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                string _rep = fbd.SelectedPath;
+                // construire la liste des fichiers
+                var _lstFic = Directory.GetFiles(_rep, "*.*", SearchOption.TopDirectoryOnly);
+                cbbFics.ItemsSource = _lstFic;
+
+                // On dispose d'une liste :
+
+                //var _res = from elt in _lstFic
+                //           where elt.Contains("doc")
+                //           select elt;
+            }
+        }
+
+        private void QuelReseau_Click(object sender, RoutedEventArgs e)
+        {
+            // Récupérer les interfaces réseau
+            var interfacesReseau = NetworkInterface.GetAllNetworkInterfaces();
+            // Construire une liste
+            var _list = from i in interfacesReseau
+                        select i.Name + " " + i.NetworkInterfaceType + " " + i.Description;
+            lstReseau.ItemsSource = _list;
+        }
+    }
+}
